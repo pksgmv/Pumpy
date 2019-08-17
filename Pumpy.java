51f0827caa1812285f1ce5cbcd08ed3d41527675
Prateek Singh <prateekgmv@gmail.com>
2019-08-17 11:33:16 +0530

    Initial Commit

---
+++ Pumpy.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Proj {
+	public static void main(String[] args) {
+		int a, b;
+		System.out.println("Enter the 1st operand: ");
+		Scanner sc = new Scanner(System.in);
+		a = sc.nextInt;
+		System.out.println("Enter the 2nd operand: ");
+		b = sc.nextInt;
+	}
+}
