5259785ee708b1c11b62e7d789bfda187061a728
Nganba-Irom <ngan17cs@cmrit.ac.in>
2019-08-17 11:57:05 +0530

    Subtract method has been added

--- Pumpy.java
+++ Pumpy.java
@@ -8,5 +8,13 @@ public class Proj {
 		a = sc.nextInt;
 		System.out.println("Enter the 2nd operand: ");
 		b = sc.nextInt;
+		subtract(a,b);
+	}
+
+	void subtract(int a,int b)
+	{
+		int res;
+		res=a-b;
+		System.out.println("Nganba says the result is "+res+" after subtraction");
 	}
 }
