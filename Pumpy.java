53a623af70b5bc1906d406a27e4798af57726be9
unknown <pumpymaisnam@gmail.com>
2019-08-17 12:09:21 +0530

    Add method added by Prerna

--- Pumpy.java
+++ Pumpy.java
@@ -1,12 +1,28 @@
 import java.util.Scanner;
 
 public class Proj {
+
+	public int add(int x,int y) {
+		
+		return x+y;
+	}
+	
+	public itsme() {
+		System.out.println("ITS ME PUMPY");
+		System.out.println(" THE BOYS CAN'T KEEP UP. ");
+	}
+	
 	public static void main(String[] args) {
 		int a, b;
 		System.out.println("Enter the 1st operand: ");
 		Scanner sc = new Scanner(System.in);
-		a = sc.nextInt;
+		a = sc.nextInt();
 		System.out.println("Enter the 2nd operand: ");
-		b = sc.nextInt;
+		b = sc.nextInt();
+		
+		itsme();
+		System.out.println("The sum is "+add(a,b));
+
+		
 	}
-}
+}                                                                                                                                                                                                                                                                                        
